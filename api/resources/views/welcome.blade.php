ff6c3621266a6a87c073cbdfec9624f0eb786fa6
Thiago Prado <tfprado2@gmail.com>
2019-01-14 21:02:19 -0500

    adding links to apps

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -85,6 +85,7 @@
                 </div>
 
                 <div class="links">
+                    <a href="http://localhost:8000">Back to OctoberCMS</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
