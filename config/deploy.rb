0362dfbdeaf932ae18ab4fc406eb12a50a3b4694
tprado <tprado@kensingtonhealth.org>
2019-01-24 16:41:26 -0500

    fixing permission errors with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,30 +7,51 @@ set :default_stage, "dev"
 set :ssh_options, {:forward_agent => true}
 
 set :application, 'kensingtoncms'
-set :repo_url, 'https://github.com/tfprado/OctoberCMS.git' 
-set :branch, ENV["branch"] || "master"
+set :repo_url, 'https://github.com/tfprado/KensingtonCMS.git' 
+set :branch, ENV["CAP_BRANCH"] || "capistrano"
 
 set :format_options, log_file: "octobercms/storage/logs/capistrano.log"
 
 namespace :deploy do
 
-    desc 'composer and october install, fixing permissions and updating/seeding databases'
-    task :october_install do
+    desc 'Copying root .env and docker-compose files, then start project with right permissions'
+    task :start_docker do
         on roles(:web) do
             within release_path do
-                execute :cp, "/var/www/octobercms/secret/.env #{release_path}/.env"
-                #execute 'composer', 'install', '--no-dev', '--optimize-autoloader'
-                execute 'sudo', 'chown', '-R', 'thedeployer:nginx', '/var/www/octobercms/releases/'
-                execute 'sudo', 'chmod', '-R', '775', '/var/www/octobercms/releases/'
-                execute 'sudo','docker-compose', '-f', '.docker/docker-compose-dev.yml', 'up', '-d', '--build'
-                execute 'sudo','docker-compose', '-f', '.docker/docker-compose-dev.yml', 'exec', '-T', 'php', 'composer', 'install'
-                execute 'sudo','docker-compose', '-f', '.docker/docker-compose-dev.yml', 'exec', '-T', 'php', 'php', 'artisan', 'october:update'
-                execute 'sudo','docker-compose', '-f', '.docker/docker-compose-dev.yml', 'exec', '-T', 'php', 'chown', '-R', 'www-data:www-data', '/var/www'
+                execute :cp, "/var/www/kensingtonCms/secret/.env-root #{release_path}/.env"
+                execute :cp, "/var/www/kensingtonCms/secret/.env-october #{release_path}/octobercms/.env"
+                execute :cp, "/var/www/kensingtonCms/secret/system.log #{release_path}/octobercms/storage/logs/system.log"
+                execute :cp, "/var/www/kensingtonCms/secret/capistrano.log #{release_path}/octobercms/storage/logs/capistrano.log"
+                execute :cp, "/var/www/kensingtonCms/secret/.env-laravel #{release_path}/api/.env"
+                execute :cp, "/var/www/kensingtonCms/secret/docker-compose.yml #{release_path}/docker-compose.yml"
+                execute 'sudo', 'chown', '-R', 'thedeployer:nginx', '/var/www/kensingtonCms/'
+                execute 'sudo', 'chmod', '-R', '775', '/var/www/kensingtonCms/'
+                execute 'sudo', 'docker-compose', 'up', '-d', '--build'
             end
         end
     end
-    
-    after "deploy:updated", 'deploy:october_install'
+
+    desc 'install laravel app'
+    task :laravel_install do
+        on roles(:web) do
+            within release_path do
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/api', '-T', 'php', 'composer', 'install'
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/api', '-T', 'php', 'php', 'artisan', 'migrate'
+            end
+        end
+    end
+
+    desc 'october install, fixing permissions and updating/seeding databases'
+    task :october_install do
+        on roles(:web) do
+            within release_path do
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/octobercms', '-T', 'php', 'composer', 'install'
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/octobercms', '-T', 'php', 'php', 'artisan', 'october:up'
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/octobercms', '-T', 'php', 'php', 'artisan', 'october:update'
+                execute 'sudo','docker-compose', 'exec', '-w', '/var/www/html/octobercms', '-T', 'php', 'chown', '-R', 'nginx:nginx', 'storage'
+            end
+        end
+    end
 
     desc 'Print The Server Name'
     task :print_server_name do
@@ -41,6 +62,9 @@ namespace :deploy do
 end
 
 after "deploy:updated", "deploy:print_server_name"
+after "deploy:updated", 'deploy:start_docker'
+after "deploy:updated", 'deploy:laravel_install'
+after "deploy:updated", 'deploy:october_install'
 
 
 
